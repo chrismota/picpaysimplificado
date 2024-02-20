4c99968aaf794507029b20123dfa3c86f313aaca
Christian Mota <christiancorreiamota@gmail.com>
2024-02-20 15:49:32 -0300

    All initial features done

---
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -0,0 +1,56 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.transaction.Transaction;
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.dtos.TransactionDTO;
+import com.picpaysimplificado.repositories.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class TransactionService {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TransactionRepository repository;
+
+    @Autowired
+    private AuthorizationService authService;
+
+    @Autowired
+    private NotificationService notificationService;
+    public Transaction createTransaction(TransactionDTO transactionDto) throws Exception {
+        User sender = this.userService.findUserById(transactionDto.senderId());
+        User receiver = this.userService.findUserById(transactionDto.receiverId());
+
+        userService.validateTransaction(sender, transactionDto.value());
+
+        boolean isAuthorized = this.authService.authorizeTransaction(sender, transactionDto.value());
+
+        if (!isAuthorized) {
+            throw new Exception("Transação não autorizada");
+        }
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setAmount(transactionDto.value());
+        newTransaction.setSender(sender);
+        newTransaction.setReceiver(receiver);
+        newTransaction.setTimestamp(LocalDateTime.now());
+
+        sender.setBalance(sender.getBalance().subtract(transactionDto.value()));
+        receiver.setBalance(receiver.getBalance().add(transactionDto.value()));
+
+        this.repository.save(newTransaction);
+        this.userService.saveUser(sender);
+        this.userService.saveUser(receiver);
+
+        this.notificationService.sendNotification(sender, "Transação realizada com sucesso.");
+        this.notificationService.sendNotification(receiver, "Transação recebida com sucesso.");
+
+        return newTransaction;
+    }
+
+}
