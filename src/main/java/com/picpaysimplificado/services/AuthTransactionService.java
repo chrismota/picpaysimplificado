07dbd573ae0ead4d7555d3bdcebf1950812428ce
Christian Mota <christiancorreiamota@gmail.com>
2024-06-27 16:55:45 -0300

    Updating transaction authorization service

--- src/main/java/com/picpaysimplificado/services/AuthTransactionService.java
+++ src/main/java/com/picpaysimplificado/services/AuthTransactionService.java
@@ -15,13 +15,15 @@ public class AuthTransactionService {
     @Autowired
     private RestTemplate restTemplate;
 
-
     public boolean authorizeTransaction(User sender, BigDecimal value) {
-        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
 
-        if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
-            String message = (String) authorizationResponse.getBody().get("message");
-            return "Autorizado".equalsIgnoreCase(message);
-        } else return false;
+        return true;
+
+//        ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc", Map.class);
+//
+//        if (authorizationResponse.getStatusCode() == HttpStatus.OK) {
+//            String message = (String) authorizationResponse.getBody().get("message");
+//            return "Autorizado".equalsIgnoreCase(message);
+//        } else return false;
     }
 }
